9a10a238199e9b60bd0b14d88ce870a3d1cc4b8c
Harry norrman <harry.norrman@gmail.com>
2023-05-03 10:11:11 +0200

    layout on frontpage changed

--- app/src/main/java/com/example/wack_a_mole/MainActivity.java
+++ app/src/main/java/com/example/wack_a_mole/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        ImageView gifImageView = findViewById(R.id.gifImageView);
+        Glide.with(this)
+                .load(R.drawable.mole)
+                .into(gifImageView);
 
     }
 }
